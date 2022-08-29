00f36572ab4d2d336b4b072c09f1c4b26f6a3bd2
Olga <olykoizumi@gmail.com>
2022-08-29 13:40:23 +0500

    p1

---
+++ p1/Program.cs
@@ -0,0 +1,15 @@
+﻿Random random = new Random();
+int randomNumber = random.Next(10, 100);
+Console.WriteLine(randomNumber);
+
+int lastNumber = randomNumber % 10;
+int firstNumber = randomNumber / 10;
+
+if(lastNumber>firstNumber)
+{
+    Console.WriteLine($"Максимальное число равно {lastNumber}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число равно {firstNumber}");
+}
